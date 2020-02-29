3e3addc278415670b7b0113e14fbc032a64d40a0
Sofian Jafar <skjafar@gmail.com>
2020-02-29 15:20:26 +0200

    Changed some more

--- goca.go
+++ goca.go
@@ -1,7 +1,7 @@
 package goca
 
 /*
-#cgo CFLAGS: -I . -I /home/sofian/epics/epics-base/include -I /home/sofian/epics/epics-bas/include/os/linux
+#cgo CFLAGS: -I . -I /home/sofian/epics/epics-base/include -I /home/sofian/epics/epics-bas/include/os/Linux
 #cgo LDFLAGS: -L/home/sofian/epics/epics-base/lib/linux-x86_64 -lca -lcom
 #include "cago.h"
 #include "cadef.h"
